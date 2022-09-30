cfa6605f82eae59179618914e90e1af4bda4955f
Benjamin Liu <benjaminliu221@gmail.com>
2022-09-29 21:55:33 -0700

    added-bubblesort-controller

---
+++ Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Controllers/BubbleSortController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OptimusPrimeWeb.Controllers
+{
+    public class BubbleSortController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
