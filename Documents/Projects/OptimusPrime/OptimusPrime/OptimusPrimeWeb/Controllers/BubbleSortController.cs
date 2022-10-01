d9c18761876f604b2850d14801b8965e556f0e8a
Benjamin Liu <benjaminliu221@gmail.com>
2022-10-01 00:58:29 -0700

    added-custom-modelstate-validations

--- Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Controllers/BubbleSortController.cs
+++ Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Controllers/BubbleSortController.cs
@@ -13,6 +13,12 @@ namespace OptimusPrimeWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> Sort(UserInput userInput)
         {
+            if (userInput.Characters.Any(char.IsDigit).Equals(false))
+            {
+                TempData["error"] = "Must contain a number(s).";
+                ModelState.AddModelError(string.Empty, TempData["error"].ToString());
+            }
+
             if (!ModelState.IsValid)
             {
                 return View();
