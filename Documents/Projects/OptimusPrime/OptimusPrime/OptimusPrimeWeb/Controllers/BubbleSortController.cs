28bb163e511d480545ac227c6b0bf581507cd614
Benjamin Liu <benjaminliu221@gmail.com>
2022-10-05 22:39:49 -0700

    Added-Validations-BubbleSort

--- Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Controllers/BubbleSortController.cs
+++ Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Controllers/BubbleSortController.cs
@@ -14,12 +14,18 @@ namespace OptimusPrimeWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> Sort(UserInput userInput)
         {
-            Regex rgx = new Regex("[0-9],");
+            Regex numRgx = new Regex("[0-9 ]");
 
-            if (rgx.IsMatch(userInput.Characters).Equals(false))
+            if (numRgx.IsMatch(userInput.Characters).Equals(false))
             {
-                TempData["Error"] = "Must contain numbers separated by a comma.";
-                ModelState.AddModelError(string.Empty, TempData["Error"].ToString());
+                TempData["NumberError"] = "Must contain number(s) separated by a space.";
+                ModelState.AddModelError(string.Empty, TempData["NumberError"].ToString());
+            }
+
+            if (Char.IsWhiteSpace(userInput.Characters, 0).Equals(true))
+            {
+                TempData["LeadingWhiteSpaceError"] = "First value must be a number NOT a space.";
+                ModelState.AddModelError(string.Empty, TempData["LeadingWhiteSpaceError"].ToString());
             }
 
             if (!ModelState.IsValid)
@@ -28,7 +34,7 @@ namespace OptimusPrimeWeb.Controllers
             }
             else
             {
-                string sortedList = Initiate(userInput.Characters).ToString();
+                string sortedList = Initiate(userInput.Characters);
                 SortedResults sortedResults = new SortedResults
                 {
                     UserInput = userInput,
@@ -38,10 +44,37 @@ namespace OptimusPrimeWeb.Controllers
             }
         }
 
-        public async Task<string> Initiate(string listOfNum)
+        public static string Initiate(string stringOfNums)
         {
-            string sortedList = "";
-            return sortedList;
+            string cleanedStringOfNums = Regex.Replace(stringOfNums, @"\s+", " ");
+            string[] strArr = cleanedStringOfNums.Split(' ');
+            int[] intArr = Array.ConvertAll(strArr, a => int.Parse(a));
+            BubbleSort(intArr);
+            string sortedNums = string.Join(",", intArr);
+            return sortedNums;
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+            {
+                for (int i = 0; i < partIndex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        Swap(array, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
