c989eb5de234e6ac3938bbb7326cf956072ece07
Benjamin Liu <benjaminliu221@gmail.com>
2022-10-07 20:55:20 -0700

    randomintegergenerator

---
+++ Documents/Projects/OptimusPrime/OptimusPrime/OptimusPrimeWeb/Models/RandomIntegerGeneratorResults.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace OptimusPrimeWeb.Models
+{
+    public class RandomIntegerGeneratorResults
+    {
+        [Required]
+        [Range(1,10000)]
+        public int LengthOfArr { get; set; }
+        [Required]
+        [Range(1,1000)]
+        public int MaxInt { get; set; }
+        [ValidateNever]
+        public string SpaceSeparatedIntegers { get; set; }
+    }
+}
